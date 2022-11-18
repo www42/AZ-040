d6ccab0956c72fbb36a37bd584322dbfb1dcdb63
www42 <thomas.jaekel@gfn.training>
2022-11-18 08:49:36 +0100

    Add startISE

---
+++ ps1/StartISE.ps1
@@ -0,0 +1,13 @@
+﻿# Start ISE as Administrator at logon 
+
+$TaskName    = "Start ISE"
+$Path        = "\"
+$AtLogon     = New-ScheduledTaskTrigger -AtLogOn
+$StartISE    = New-ScheduledTaskAction -Execute PowerShell_ISE.exe
+$CurrentUser = New-ScheduledTaskPrincipal -RunLevel Highest -LogonType Interactive -UserId $env:USERNAME
+
+Register-ScheduledTask -TaskName  $TaskName `
+                       -TaskPath  $Path `
+                       -Trigger   $AtLogon `
+                       -Principal $CurrentUser `
+                       -Action    $StartISE
