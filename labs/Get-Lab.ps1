4f7cf77d2619ec6c2978245573529db153d147e0
www42 <tjaekel0815@gmail.com>
2026-01-27 16:45:57 +0100

    Test

--- labs/Get-Lab.ps1
+++ labs/Get-Lab.ps1
@@ -5,9 +5,14 @@ function Get-Lab {
       [ValidateSet("1","2","3","4","5","6","7","8","9","10","11")]
       [string]$Lab
     )
-        $Url = "https://raw.githubusercontent.com/www42/AZ-040/master/Labs/Lab$Lab.ps1"
+    $Url = "https://raw.githubusercontent.com/www42/AZ-040/master/Labs/Lab$Lab.ps1"
     $File = "$env:TEMP\Lab$Lab.ps1"
     Invoke-WebRequest -UseBasicParsing -Uri $Url | % Content > $File
     psEdit $File
 }
 Get-Lab
+
+$Lab = '3'
+$Url = "https://raw.githubusercontent.com/www42/AZ-040/master/Labs/Lab$Lab.ps1"
+$Url = "https://raw.githubusercontent.com/www42/az-040/master/labs/Lab3.ps1"
+Invoke-WebRequest -UseBasicParsing -Uri $Url | % Content
