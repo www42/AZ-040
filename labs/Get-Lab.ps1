6cd5bb47ce70cd0cba474e369a7986e53d9d2896
www42 <tjaekel0815@gmail.com>
2026-01-27 16:27:15 +0100

    Add file

---
+++ labs/Get-Lab.ps1
@@ -0,0 +1,13 @@
+function Get-Lab {
+    [CmdletBinding()]
+    Param ( 
+      [Parameter(Mandatory=$true, Position=0)]
+      [ValidateSet("1","2","3","4","5","6","7","8","9","10","11")]
+      [string]$Lab
+    )
+        $Url = "https://raw.githubusercontent.com/www42/AZ-040/master/Labs/Lab$Lab.ps1"
+    $File = "$env:TEMP\Lab$Lab.ps1"
+    Invoke-WebRequest -UseBasicParsing -Uri $Url | % Content > $File
+    psEdit $File
+}
+Get-Lab
